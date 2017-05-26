cf6fcd4f19ae033c234e67f11feb221ea22f5ab2
Zhyldyza <jyldyzakylbekova@gmail.com>
2017-05-26 23:56:20 +0300

    Add files via upload
    entities

---
+++ Subject.java
@@ -0,0 +1,22 @@
+package android.kalys.androidisaev.entities;
+
+/**
+ */
+
+public class Subject {
+    private String subjectName;
+
+    public Subject(){}
+
+    public Subject(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+}
---
+++ User.java
@@ -0,0 +1,104 @@
+package android.kalys.androidisaev.entities;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * */
+
+public class User {
+    private String firsName;
+    private String lastName;
+    private Group group;
+    private String email;
+    private List<Float> marks;
+
+    public User(){
+        marks = new LinkedList<>();
+    }
+
+    public User(String firsName, String lastName, Group group, String email) {
+        this.firsName = firsName;
+        this.lastName = lastName;
+        this.group = group;
+        this.email = email;
+        marks = new LinkedList<>();
+    }
+
+    public String getFirsName() {
+        return firsName;
+    }
+
+    public void setFirsName(String firsName) {
+        this.firsName = firsName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Float> getMarks() {
+        return marks;
+    }
+
+    public void setMarks(List<Float> marks) {
+        this.marks = marks;
+    }
+
+    public float countGPA(){
+        float gpa = 0;
+        for(float temp : marks){
+            gpa += temp;
+        }
+        gpa = gpa/marks.size();
+
+        return gpa;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (firsName != null ? !firsName.equals(user.firsName) : user.firsName != null)
+            return false;
+        if (lastName != null ? !lastName.equals(user.lastName) : user.lastName != null)
+            return false;
+        if (group != null ? !group.equals(user.group) : user.group != null) return false;
+        if (email != null ? !email.equals(user.email) : user.email != null) return false;
+        return marks != null ? marks.equals(user.marks) : user.marks == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firsName != null ? firsName.hashCode() : 0;
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (group != null ? group.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (marks != null ? marks.hashCode() : 0);
+        return result;
+    }
+}
